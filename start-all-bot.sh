e67e88988b07997e092703848025ff196b88d156
Michael Graf (home) <mgraf@sqli.com>
2016-07-31 21:15:00 +0200

    virtualenv

--- start-all-bot.sh
+++ start-all-bot.sh
@@ -4,6 +4,8 @@
 rm -Rf PokemonGo-Bot
 git clone --recursive -b master https://github.com/Onigam/PokemonGo-Bot PokemonGo-Bot
 cd PokemonGo-Bot
+virtualenv .
+source bin/activate
 pip install -r requirements.txt
 mv release_config.json.example release_config.json
 mkdir logs
