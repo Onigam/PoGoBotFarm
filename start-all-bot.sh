5c9ed800656c0b75a3879008dcb2e54a27d731fa
Michael Graf (home) <mgraf@sqli.com>
2016-07-31 18:19:32 +0200

    chmod

--- start-all-bot.sh
+++ start-all-bot.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+rm -Rf PokemonGo-Bot
+git clone https://github.com/Onigam/PokemonGo-Bot PokemonGo-Bot
+cd PokemonGo-Bot
+
 cat users.csv | while read line
 do
         USERNAME=$(echo "$line" | cut -d';' -f1)    # get the user name
         PASSWORD=$(echo "$line" | cut -d';' -f2)    # get the password
         LOCATION=$(echo "$line" | cut -d';' -f3)    # get the location
         echo "$USERNAME $PASSWORD $LOCATION"
-        sh ./start-bot.sh -u=$USERNAME -p=$PASSWORD -l=$LOCATION
+        ./start-bot.sh -u=$USERNAME -p=$PASSWORD -l=$LOCATION
 done
--- start-bot.sh
+++ start-bot.sh
@@ -25,11 +25,7 @@ echo USER = ${USER}
 echo PASSWORD = ${PASSWORD}
 echo LOCATION = ${LOCATION}
 
-rm -Rf PokemonGo-Bot-${USER}
-git clone https://github.com/Onigam/PokemonGo-Bot PokemonGo-Bot-${USER}
-cd PokemonGo-Bot-${USER}
-touch config.json
-echo "{\"walk\": 8,\"auth_service\":\"ptc\", \"username\":\"${USER}\", \"password\":\"${PASSWORD}\", \"location\":\"${LOCATION}\", \"gmapkey\":\"AIzaSyAM63b9XvA6TeyrSTji5rWBLE_6eJxZgwU\", \"max_steps\":5, \"test\": false, \"initial_transfer\": 0, \"location_cache\": true, \"distance_unit\": \"km\", \"item_filter\": \"101,102,103,104\",\"evolve_all\": \"all\"}" > config.json
+cp ../config.json .
 mv release_config.json.example release_config.json
 pwd
 cp ../init.sh init.sh
