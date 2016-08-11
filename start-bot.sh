921376b49bcd391fe811886c1bdfd41399f847cd
Michael Graf <mgraf@sqli.com>
2016-08-11 15:33:16 +0200

    merci Magino

--- start-bot.sh
+++ start-bot.sh
@@ -31,4 +31,5 @@ sed -i "s/ACCOUNT/${USER}/g" config-${USER}.json
 sed -i "s/PWD/${PASSWORD}/g" config-${USER}.json
 sed -i "s/LOCATION/${LOCATION}/g" config-${USER}.json
 sed -i "s/GMAPKEY/AIzaSyAM63b9XvA6TeyrSTji5rWBLE_6eJxZgwU/g" config-${USER}.json
+nohup forever --spinSleepTime 10000 --minUptime 9999999999999999999 -c python pokecli.py --config config-${USER}.json > logs/log-${USER}.log 2>&1&
 echo Bot ${USER} Started
